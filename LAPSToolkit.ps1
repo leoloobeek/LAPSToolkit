1431454fbd26d84915e1bfcf4ba4a6c93bc901cd
lloobeek <lloobeek@issgs.net>
2016-07-30 09:47:43 -0500

    Created computer map and sid map to resolve sids while using PSCredential object. Added Get-NetUser for sid map.

--- LAPSToolkit.ps1
+++ LAPSToolkit.ps1
@@ -2528,6 +2528,158 @@ function Get-NetGroupMember {
     }
 }
 
+function Get-NetUser {
+<#
+    .SYNOPSIS
+
+        Query information for a given user or users in the domain
+        using ADSI and LDAP. Another -Domain can be specified to
+        query for users across a trust.
+        Replacement for "net users /domain"
+
+    .PARAMETER UserName
+
+        Username filter string, wildcards accepted.
+
+    .PARAMETER Domain
+
+        The domain to query for users, defaults to the current domain.
+
+    .PARAMETER DomainController
+
+        Domain controller to reflect LDAP queries through.
+
+    .PARAMETER ADSpath
+
+        The LDAP source to search through, e.g. "LDAP://OU=secret,DC=testlab,DC=local"
+        Useful for OU queries.
+
+    .PARAMETER Filter
+
+        A customized ldap filter string to use, e.g. "(description=*admin*)"
+
+    .PARAMETER AdminCount
+
+        Switch. Return users with adminCount=1.
+
+    .PARAMETER SPN
+
+        Switch. Only return user objects with non-null service principal names.
+
+    .PARAMETER Unconstrained
+
+        Switch. Return users that have unconstrained delegation.
+
+    .PARAMETER AllowDelegation
+
+        Switch. Return user accounts that are not marked as 'sensitive and not allowed for delegation'
+
+    .PARAMETER PageSize
+
+        The PageSize to set for the LDAP searcher object.
+
+    .PARAMETER Credential
+
+        A [Management.Automation.PSCredential] object of alternate credentials
+        for connection to the target domain.
+
+    .EXAMPLE
+
+        PS C:\> Get-NetUser -Domain testing
+
+    .EXAMPLE
+
+        PS C:\> Get-NetUser -ADSpath "LDAP://OU=secret,DC=testlab,DC=local"
+#>
+
+    param(
+        [Parameter(Position=0, ValueFromPipeline=$True)]
+        [String]
+        $UserName,
+
+        [String]
+        $Domain,
+
+        [String]
+        $DomainController,
+
+        [String]
+        $ADSpath,
+
+        [String]
+        $Filter,
+
+        [Switch]
+        $SPN,
+
+        [Switch]
+        $AdminCount,
+
+        [Switch]
+        $Unconstrained,
+
+        [Switch]
+        $AllowDelegation,
+
+        [ValidateRange(1,10000)] 
+        [Int]
+        $PageSize = 200,
+
+        [Management.Automation.PSCredential]
+        $Credential
+    )
+
+    begin {
+        # so this isn't repeated if users are passed on the pipeline
+        $UserSearcher = Get-DomainSearcher -Domain $Domain -ADSpath $ADSpath -DomainController $DomainController -PageSize $PageSize -Credential $Credential
+    }
+
+    process {
+        if($UserSearcher) {
+
+            # if we're checking for unconstrained delegation
+            if($Unconstrained) {
+                Write-Verbose "Checking for unconstrained delegation"
+                $Filter += "(userAccountControl:1.2.840.113556.1.4.803:=524288)"
+            }
+            if($AllowDelegation) {
+                Write-Verbose "Checking for users who can be delegated"
+                # negation of "Accounts that are sensitive and not trusted for delegation"
+                $Filter += "(!(userAccountControl:1.2.840.113556.1.4.803:=1048574))"
+            }
+            if($AdminCount) {
+                Write-Verbose "Checking for adminCount=1"
+                $Filter += "(admincount=1)"
+            }
+
+            # check if we're using a username filter or not
+            if($UserName) {
+                # samAccountType=805306368 indicates user objects
+                $UserSearcher.filter="(&(samAccountType=805306368)(samAccountName=$UserName)$Filter)"
+            }
+            elseif($SPN) {
+                $UserSearcher.filter="(&(samAccountType=805306368)(servicePrincipalName=*)$Filter)"
+            }
+            else {
+                # filter is something like "(samAccountName=*blah*)" if specified
+                $UserSearcher.filter="(&(samAccountType=805306368)$Filter)"
+            }
+
+            $Results = $UserSearcher.FindAll()
+            $Results | Where-Object {$_} | ForEach-Object {
+                # convert/process the LDAP fields for each result
+                $User = Convert-LDAPProperty -Properties $_.Properties
+                $User.PSObject.TypeNames.Add('PowerView.User')
+                $User
+            }
+            $Results.dispose()
+            $UserSearcher.dispose()
+        }
+    }
+}
+
+
+
 
 ########################################################
 #
@@ -2570,6 +2722,8 @@ function Find-AdmPwdExtendedRights {
         A [Management.Automation.PSCredential] object of alternate credentials
         for connection to the target domain.
 
+        NOTE: You must use FQDN of domain: testlab.local\user, not testlab\user
+
     .EXAMPLE
 
         PS C:\> Find-AdmPwdExtendedRights
@@ -2586,6 +2740,14 @@ function Find-AdmPwdExtendedRights {
     .EXAMPLE
 
         PS C:\> Find-AdmPwdExtendedRights -ComputerName victim1.testlab.local
+        
+        Description
+        -----------
+        Only retrieves ExtendedRights for the computer specified
+
+    .EXAMPLE
+
+        PS C:\> Find-AdmPwdExtendedRights -Credential testlab.local\user -Domain testlab.local -DomainController 192.168.1.1
         
         Description
         -----------
@@ -2629,17 +2791,27 @@ function Find-AdmPwdExtendedRights {
             $LAPSFilter = "$Filter"
         }
 
+        Write-Verbose "Retrieving all ExtendedRight ACLs for domain $Domain"
         $ExtendedRights = Get-ObjectAcl -ResolveGUIDs -Filter $LAPSFilter -Domain $Domain -DomainController $DomainController -Credential $Credential -PageSize $PageSize | Where-Object { $_.ActiveDirectoryRights -match "ExtendedRight" }
-        # Get all computers objects once to reference below
-        $ComputerObjects = Get-NetComputer -Filter "(ms-mcs-admpwdexpirationtime=*)" -FullData -Domain $Domain -DomainController $DomainController -Credential $Credential
+        
+        # Get all computers objects once, then reference below
+        $CompMap = @{}
+        $ComputerObjects = Get-NetComputer -Filter "(ms-mcs-admpwdexpirationtime=*)" -FullData -Domain $Domain -DomainController $DomainController -Credential $Credential | ForEach-Object { $CompMap.Add($_.distinguishedname, $_.dnshostname) }
+
+        if($Credential){
+            Write-Verbose "Retrieving all users and groups to resolve SIDs when using PSCredential"
+            $SIDMap = @{}
+            Get-NetUser -Domain $Domain -DomainController $DomainController -Credential $Credential | ForEach-Object { $SIDMap.Add($_.objectsid, $_.samaccountname) }
+            Get-NetGroup -FullData -Domain $Domain -DomainController $DomainController -Credential $Credential | ForEach-Object { $SIDMap.Add($_.objectsid, $_.samaccountname) }
+        }
     }
     process {
 
         $ExtendedRights | ForEach-Object {
-            $dn = $_.ObjectDN
-            $ComputerName =  $ComputerObjects | Where-Object { $_.distinguishedname -match $dn } | Select-Object dnshostname -ExpandProperty dnshostname
-            Write-Verbose "Adding data for $ComputerName"
-            $Identity = $_.IdentityReference
+
+            $ComputerName =  $CompMap[$_.ObjectDN]
+            Write-Verbose "Parsing ACLs for $ComputerName"
+            $ir = $_.IdentityReference
 
             if($_.ObjectType -match "ms-Mcs-AdmPwd") {
                 $Reason = "Delegated"
@@ -2648,6 +2820,13 @@ function Find-AdmPwdExtendedRights {
                 $Reason = "All"
             }
             else { return }
+
+            if($Credential) {
+                $Identity = $SIDMap[$ir.ToString()]
+            }
+            else { 
+                $Identity = $ir 
+            }
 
             $ExtendedRightUser = New-Object PSObject
             $ExtendedRightUser | Add-Member Noteproperty 'ComputerName' "$ComputerName"
